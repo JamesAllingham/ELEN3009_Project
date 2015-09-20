7c82d0f621c05c1a09de573f9dbf40d308409b00
Pcres91 <paulcresswell@live.com>
2015-09-20 21:59:46 +0200

    Tests for Flyer class created
    Tested numberOfFlyers static int increasing

---
+++ Test/Flyer_unit_tests.cpp
@@ -0,0 +1,23 @@
+#include <ctime>
+#include <cmath>
+#include <memory>
+using std::shared_ptr;
+using std::make_shared;
+
+#include <iostream> // For testing purposes
+
+#include "Character.h"
+#include "Entity.h"
+#include "Missile.h"
+#include "Flyer.h"
+
+#include "gtest/gtest.h"
+
+TEST(Flyer, NumberOfFlyersIncreasesWithNewlyCreatedFlyers)
+{
+	shared_ptr<Flyer> Flyer1 (new Flyer);
+	shared_ptr<Flyer> Flyer2 (new Flyer);
+	shared_ptr<Flyer> Flyer3 (new Flyer);
+	
+	EXPECT_EQ(3, Flyer::numberOfFlyers());
+}
