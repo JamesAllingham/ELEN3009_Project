7a1808835fac2aed7808230e1a31601e2034822f
JamesAllingham <james.allingham@gmail.com>
2015-10-03 22:19:32 +0200

    Small changes to ResourceHolder tests

--- Test/ResourceHolder_unit_tests.cpp
+++ Test/ResourceHolder_unit_tests.cpp
@@ -17,8 +17,8 @@ TEST(ResourceHolder, loadWithTwoParametersThrowsCorrectly)
 	
 	ResourceHolder<Texture,TestID> testTextures;
 	
-	EXPECT_THROW(testTextures.load(TestID::Test1, "resources/test1.jpg"),runtime_error);
-	EXPECT_NO_THROW(testTextures.load(TestID::Test2, "resources/test.jpg"));
+	EXPECT_THROW(testTextures.load(TestID::Test1, "resources/test.jpg"),runtime_error);
+	EXPECT_NO_THROW(testTextures.load(TestID::Test2, "resources/test1.jpg"));
 }
 
 TEST(ResourceHolder, loadWithThreeParametersThrowsCorrectly)
@@ -31,13 +31,14 @@ TEST(ResourceHolder, loadWithThreeParametersThrowsCorrectly)
 	
 	ResourceHolder<Texture,TestID> testTextures;
 	
-	EXPECT_THROW(testTextures.load(TestID::Test1, "resources/test1.jpg",IntRect(10, 10, 32, 32)),runtime_error);
-	EXPECT_NO_THROW(testTextures.load(TestID::Test2, "resources/test.jpg",IntRect(10, 10, 32, 32)));
+	EXPECT_THROW(testTextures.load(TestID::Test1, "resources/test.jpg",IntRect(10, 10, 32, 32)),runtime_error);
+	EXPECT_NO_THROW(testTextures.load(TestID::Test2, "resources/test1.jpg",IntRect(10, 10, 32, 32)));
 }
 
 TEST(ResourceHolder, nonConstGetFunctionReturnsCorrectResource)
 {
-	string file_path_and_name = "resources/test.jpg";
+	string file_path_and_name1 = "resources/test1.jpg";
+	string file_path_and_name2 = "resources/test2.jpg";
 	
 	enum class TestID
 	{
@@ -47,10 +48,11 @@ TEST(ResourceHolder, nonConstGetFunctionReturnsCorrectResource)
 	
 	ResourceHolder<Texture,TestID> testTextures;
 	
-	testTextures.load(TestID::Test1, file_path_and_name);
+	testTextures.load(TestID::Test1, file_path_and_name1);
+	testTextures.load(TestID::Test2, file_path_and_name2);
 	
 	Texture testTexture;
-	testTexture.loadFromFile(file_path_and_name);
+	testTexture.loadFromFile(file_path_and_name1);
 	
 	// This might not be a good way to test this - not sure
 	EXPECT_EQ(testTexture.getSize(),testTextures.get(TestID::Test1).getSize());	
@@ -58,7 +60,8 @@ TEST(ResourceHolder, nonConstGetFunctionReturnsCorrectResource)
 
 TEST(ResourceHolder, constGetFunctionReturnsCorrectResource)
 {
-	string file_path_and_name = "resources/test.jpg";
+	string file_path_and_name1 = "resources/test1.jpg";
+	string file_path_and_name2 = "resources/test2.jpg";
 	
 	enum class TestID
 	{
@@ -67,11 +70,12 @@ TEST(ResourceHolder, constGetFunctionReturnsCorrectResource)
 	};
 	
 	ResourceHolder<Texture,TestID> testTextures;	
-	testTextures.load(TestID::Test1, file_path_and_name);
+	testTextures.load(TestID::Test1, file_path_and_name1);	
+	testTextures.load(TestID::Test2, file_path_and_name2);
 	const auto * const_textures_ptr = &testTextures;
 	
 	Texture testTexture;
-	testTexture.loadFromFile(file_path_and_name);
+	testTexture.loadFromFile(file_path_and_name1);
 	
 	const Texture testTextureConst = const_textures_ptr->get(TestID::Test1);
 	
